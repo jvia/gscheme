5fd616deada89414d37ce2982c2b70980ff2789c
Jeremiah Via <jeremiah.via@gmail.com>
2013-12-11 17:44:34 -0500

    Project done...

--- src/11.scm
+++ src/11.scm
@@ -10,12 +10,6 @@
                         #t
                         ((@ exists? 'a) p? ((@ cdr 'a) l)))))))
 
-(define bool eq2 ((int x))
-  ((@ = int) x 2))
-
-
-
-
 ;; all?
 (val-rec
  (forall ('a ) (function ((function ('a) bool) (list 'a)) bool))
@@ -25,20 +19,3 @@
                 (let ((notP? ((@ o 'a bool bool) not p?)))
                   (not ((@ exists? 'a) notP? l))))))
 
-
-
-(let ((existsint? (@ exists? int))
-      (allint?    (@ all? int))
-      (lst1 '(1 2 3 4 5))
-      (lst2 '(2 2 2 2 2))
-      (lst3 '(5)))
-  ((@ list3 (list bool))
-   ((@ list2 bool)
-    (existsint? eq2 lst1)
-    (allint? eq2 lst1))
-   ((@ list2 bool)
-    (existsint? eq2 lst2)
-    (allint? eq2 lst2))
-   ((@ list2 bool)
-    (existsint? eq2 lst3)
-    (allint? eq2 lst3))))

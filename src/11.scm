a51eabea62baae0f10bc3d5a4647ec7247ba5bb0
Jeremiah Via <jeremiah.via@gmail.com>
2013-12-11 17:43:25 -0500

    Add all? function.

--- src/11.scm
+++ src/11.scm
@@ -1,34 +1,44 @@
 ;; exists?
-(val-rec (forall ('a ) (function ((function ('a) bool) (list 'a)) bool)) exists?
-         (type-lambda ('a)
-                 (lambda (((function ('a) bool) p?)  ((list 'a) l))
-                   (if ((@ null? 'a) l)
-                       #f
-                       (if (p? ((@ car 'a) l))                           
-                           #t
-                           ((@ exists? 'a) p? ((@ cdr 'a) l)))))))
+(val-rec
+ (forall ('a ) (function ((function ('a) bool) (list 'a)) bool))
+ exists?
+ (type-lambda ('a)
+              (lambda (((function ('a) bool) p?)  ((list 'a) l))
+                (if ((@ null? 'a) l)
+                    #f
+                    (if (p? ((@ car 'a) l))                           
+                        #t
+                        ((@ exists? 'a) p? ((@ cdr 'a) l)))))))
 
 (define bool eq2 ((int x))
   ((@ = int) x 2))
 
-(let ((existsint? (@ exists? int)))
-  (existsint? eq2 '(1 3 3 4 3 1)))
+
+
 
 ;; all?
+(val-rec
+ (forall ('a ) (function ((function ('a) bool) (list 'a)) bool))
+ all?
+ (type-lambda ('a)
+              (lambda (((function ('a) bool) p?) ((list 'a) l))
+                (let ((notP? ((@ o 'a bool bool) not p?)))
+                  (not ((@ exists? 'a) notP? l))))))
 
 
-;; (val-rec (forall ('a) (function ((function ('a) bool) (list 'a)) (list 'a))) filter
-;;          (type-lambda ('a)
-;;                  (lambda (((function ('a) bool) p?)  ((list 'a) l))
-;;                    (if ((@ null? 'a) l)
-;;                        (@ '() 'a)
-;;                        (if (p? ((@ car 'a) l))                           
-;;                            ((@ cons 'a) ((@ car 'a) l) ((@ filter 'a) p? ((@ cdr 'a) l)))
-;;                            ((@ filter 'a) p? ((@ cdr 'a) l)))))))
 
-;; (define filter (p? xs)
-;;   (if (null? xs)
-;;       '()
-;;       (if (p? (car xs))
-;;           (cons (car xs) (filter p? (cdr xs)))
-;;           (filter p? (cdr xs)))))
+(let ((existsint? (@ exists? int))
+      (allint?    (@ all? int))
+      (lst1 '(1 2 3 4 5))
+      (lst2 '(2 2 2 2 2))
+      (lst3 '(5)))
+  ((@ list3 (list bool))
+   ((@ list2 bool)
+    (existsint? eq2 lst1)
+    (allint? eq2 lst1))
+   ((@ list2 bool)
+    (existsint? eq2 lst2)
+    (allint? eq2 lst2))
+   ((@ list2 bool)
+    (existsint? eq2 lst3)
+    (allint? eq2 lst3))))
